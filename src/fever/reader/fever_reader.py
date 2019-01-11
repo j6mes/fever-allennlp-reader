55098f3e9b19af71a0cb184f06edca51a7e173a7
James Thorne <james@jthorne.co.uk>
2019-01-11 11:25:54 +0000

    made additional functionality for preprocsesing script to generate multiple instances per line

--- src/fever/reader/fever_reader.py
+++ src/fever/reader/fever_reader.py
@@ -4,12 +4,12 @@ from typing import Dict, Union, Iterable, List, Tuple
 
 from allennlp.data import Tokenizer, TokenIndexer, Instance
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
-from allennlp.data.fields import TextField
+from allennlp.data.fields import TextField, LabelField
 from allennlp.data.token_indexers import SingleIdTokenIndexer
 from allennlp.data.tokenizers import WordTokenizer
 
 from fever.reader.document_database import FEVERDocumentDatabase
-from fever.reader.preprocessing import FEVERPreprocessing, ConcatenateEvidence
+from fever.reader.preprocessing import FEVERInstanceGenerator, ConcatenateEvidence
 from fever.reader.simple_random import SimpleRandom
 
 
@@ -18,9 +18,11 @@ from fever.reader.simple_random import SimpleRandom
 @DatasetReader.register("fever")
 class FEVERDatasetReader(DatasetReader):
     
-    def __init__(self, database: Union[FEVERDocumentDatabase, str], wiki_tokenizer: Tokenizer = None,
-                 claim_tokenizer: Tokenizer = None, token_indexers: Dict[str, TokenIndexer] = None,
-                 preprocessing: FEVERPreprocessing = ConcatenateEvidence()) -> None:
+    def __init__(self, database: Union[FEVERDocumentDatabase, str],
+                 wiki_tokenizer: Tokenizer = None,
+                 claim_tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 instance_generator: FEVERInstanceGenerator = None) -> None:
 
         super().__init__()
         if type(database) == str:
@@ -30,7 +32,7 @@ class FEVERDatasetReader(DatasetReader):
         self._wiki_tokenizer = wiki_tokenizer or WordTokenizer()
         self._claim_tokenizer = claim_tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
-        self._preprocessing = preprocessing or FEVERPreprocessing()
+        self._instance_generator = instance_generator or ConcatenateEvidence()
 
     def get_doc_lines(self, page_title:str) -> List[str]:
         doc_lines = self._database.get_doc_lines(page_title)
@@ -52,18 +54,33 @@ class FEVERDatasetReader(DatasetReader):
         return [line for line in lines if len(line.strip())]
 
     @overrides
-    def text_to_instance(self, evidence, claim:str) -> Instance:
-
-        evidence = [[self.get_doc_line(item[2], item[3]) for item in group] for group in evidence]
-
-        evidence, claim = self._preprocessing.preprocess(evidence, claim)
+    def text_to_instance(self, evidence:str, claim:str, label:str = None) -> Instance:
 
         claim_tokens = self._claim_tokenizer.tokenize(claim)
         evidence_tokens = self._wiki_tokenizer.tokenize(evidence)
 
-        return Instance(
-            {"premise": TextField(evidence_tokens, self._token_indexers),
-             "hypothesis": TextField(claim_tokens, self._token_indexers)})
+        instance_dict = {"premise": TextField(evidence_tokens, self._token_indexers),
+                         "hypothesis": TextField(claim_tokens, self._token_indexers),
+                        }
+
+
+        if label is not None:
+            instance_dict["label"] = LabelField(label)
+
+        return Instance(instance_dict)
+
+
+    def generate_instances(self,
+                           evidence: List[List[Tuple[str,int]]],
+                           claim: str,
+                           label:str = None) -> Iterable[Instance]:
+
+        generated = self._instance_generator.generate_instances(self, evidence, claim)
+        return [self.text_to_instance(item['evidence'], item['claim'],label) for item in generated]
+
+    """
+    self.text_to_instance(evidence, claim)
+    """
 
     @overrides
     def read(self, file_path:str) -> Iterable[Instance]:
@@ -72,7 +89,10 @@ class FEVERDatasetReader(DatasetReader):
             for line in f:
                 instance = json.loads(line)
 
-                claim = instance['claim']
-                evidence = instance['evidence']
+                claim:str = instance['claim']
+                evidence: List[List[Tuple[int,int,str,int]]] = instance['evidence']
+                evidence: List[List[Tuple[str,int]]] = [[(item[2], item[3]) for item in group] for group in evidence]
 
-                yield from [self.text_to_instance(evidence, claim)]
+                label:str = instance['label'] if 'label' in instance else None
+
+                yield from self.generate_instances(evidence, claim, label)
--- src/fever/reader/preprocessing.py
+++ src/fever/reader/preprocessing.py
@@ -3,28 +3,28 @@ from typing import List, Tuple
 from allennlp.common import Registrable
 from overrides import overrides
 
+class FEVERInstanceGenerator(Registrable):
 
-class FEVERPreprocessing(Registrable):
-
-    def preprocess(self, evidence:List[List[str]], claim:str)->Tuple[str,str]:
+    def generate_instances(self, reader, evidence:List[List[Tuple[str, int]]], claim:str):
         raise NotImplemented("This preprocessing function should be implemented")
 
 
-@FEVERPreprocessing.register("concatenate")
-class ConcatenateEvidence(FEVERPreprocessing):
+@FEVERInstanceGenerator.register("concatenate")
+class ConcatenateEvidence(FEVERInstanceGenerator):
 
     @staticmethod
     def _flatten(l):
         return [item for sublist in l for item in sublist]
 
     @overrides
-    def preprocess(self, evidence:List[List[str]], claim:str)->Tuple[str,str]:
-        items = self._flatten(evidence)
+    def generate_instances(self, reader, evidence:List[List[Tuple[str, int]]], claim:str):
+        evidence_text: List[List[str]] = [[reader.get_doc_line(item[0],item[1]) for item in group] for group in evidence]
+        flat_evidence_text: List[str] = self._flatten(evidence_text)
 
         # Key sets in dictionaries preserve insert order whereas python sets do not
-        evidence = dict()
-        for item in items:
-            evidence[item] = 1
-        evidence = " ".join(evidence.keys())
-        return evidence, claim
+        evidence_dict = dict()
+        for item in flat_evidence_text:
+            evidence_dict[item] = 1
+        evidence = " ".join(evidence_dict.keys())
+        return [{"evidence":evidence, "claim":claim}]
 
