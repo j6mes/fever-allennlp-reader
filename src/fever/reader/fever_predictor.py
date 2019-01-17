a6ec3d71e2a8b893bb2280095aba7f837dde5aba
James Thorne <james@jthorne.co.uk>
2019-01-17 22:24:21 +0000

    add function to load json line

--- src/fever/reader/fever_predictor.py
+++ src/fever/reader/fever_predictor.py
@@ -23,11 +23,14 @@ class FEVERPredictor(Predictor):
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
         claim_id: int = json_dict['id']
         claim: str = json_dict['claim']
+        label: str = json_dict['label'] if 'label' in json_dict else None
         evidence: List[List[Tuple[str, int]]] = json_dict['predicted_sentences']
 
-        return self.text_to_instance(claim_id, None, evidence, claim, None)
+        return self.text_to_instance(claim_id, None, evidence, claim, label)
 
-    
+    def predict(self, json_line:str) -> JsonDict:
+        return self.predict_json(json.loads(json_line))
+
     def __init__(self, model: Model, dataset_reader: DatasetReader) -> None:
         super().__init__(model, dataset_reader)
 
